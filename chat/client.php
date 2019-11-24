38434b0f06239afcd1ef5223a35a83a09a115ca3
Artur Quaresma <artursquaresma@gmail.com>
2019-11-24 10:27:37 +0000

    Update client.php

--- chat/client.php
+++ chat/client.php
@@ -4,6 +4,10 @@ set_time_limit(0); //Mete o limite de resposta como 0
 
 function emojiMenu(){
 	return "
+	_  _ ____ _  _ _  _    ____ _  _ ____  _ _ ____    
+	|\/| |___ |\ | |  |    |___ |\/| |  |  | | [__     
+	|  | |___ | \| |__|    |___ |  | |__| _| | ___]    
+                                                   
 		Digite o comando para obter o emoji\n\n
 
 		_sad --> ( ͡° ʖ̯ ͡°) \n
@@ -41,10 +45,10 @@ echo "
 ╚██████╗██║  ██║██║  ██║   ██║       ██║     ██║  ██║██║     
 ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝       ╚═╝     ╚═╝  ╚═╝╚═╝ ";
 echo "\n\n║═════════════════════════════════════\n";
-echo emojiMenu();
 
 //array de palavras de baixo calao
-$badwords = array( "anus" => "a____", "bastardo" => "b____", "bicha" => "b____", "buceta" => "b____", "burra" => "b____", "burro" => "b____", "cadela" => "c____", "cadelo" => "c____", "caralho" => "c____", "cassete" => "c____", "cona" => "c____", "chupado" => "c____", "chupado" => "c____", "cocaina" => "c____", "corno" => "c____", "cornuda" => "c____", "cornudo" => "c____", "colhao" => "c____", "doida" => "d____", "doido" => "d____", "escrota" => "e____", "escroto" => "e____", "feia" => "f____", "feio" => "f____", "fodas" => "f____", "foda" => "f____", "fudida" => "f____", "fudido" => "f____", "fudendo" => "f____", "fudida" => "f____", "fudido" => "f____", "furnicar" => "f____", "foda-se" => "f____", "foda" => "f____", "gay" => "g____", "homosexual" => "h____", "homossexual" => "h____", "idiota" => "i____", "leprosa" => "l____", "leproso" => "l____", "lesbica" => "l____", "maconha" => "m____", "moleque" => "m____", "paspalha" => "p____", "paspalhao" => "p____", "paspalho" => "p____", "pau" => "p____", "penis" => "p____", "pentelho" => "p____", "piça" => "p____", "pica" => "p____", "piroca," => "p____", "piropo" => "p____", "prostituta" => "p____", "prostituto" => "p____", "puta" => "p____", "retardada" => "r____", "retardado" => "r____", "sacana" => "s____", "vaca" => "v___", "vagina" => "v____"
+$badwords = array(
+	"anus" => "a____", "bastardo" => "b____", "bicha" => "b____", "buceta" => "b____", "burra" => "b____", "burro" => "b____", "cadela" => "c____", "cadelo" => "c____", "caralho" => "c____", "cassete" => "c____", "cona" => "c____", "chupado" => "c____", "chupado" => "c____", "cocaina" => "c____", "corno" => "c____", "cornuda" => "c____", "cornudo" => "c____", "colhao" => "c____", "doida" => "d____", "doido" => "d____", "escrota" => "e____", "escroto" => "e____", "feia" => "f____", "feio" => "f____", "fodas" => "f____", "foda" => "f____", "fudida" => "f____", "fudido" => "f____", "fudendo" => "f____", "fudida" => "f____", "fudido" => "f____", "furnicar" => "f____", "foda-se" => "f____", "foda" => "f____", "gay" => "g____", "homosexual" => "h____", "homossexual" => "h____", "idiota" => "i____", "leprosa" => "l____", "leproso" => "l____", "lesbica" => "l____", "maconha" => "m____", "moleque" => "m____", "paspalha" => "p____", "paspalhao" => "p____", "paspalho" => "p____", "pau" => "p____", "penis" => "p____", "pentelho" => "p____", "piça" => "p____", "pica" => "p____", "piroca," => "p____", "piropo" => "p____", "prostituta" => "p____", "prostituto" => "p____", "puta" => "p____", "retardada" => "r____", "retardado" => "r____", "sacana" => "s____", "vaca" => "v___", "vagina" => "v____"
 );
 
 //array de emojis
@@ -53,7 +57,7 @@ $emojis = array(
 	"_happy" => "\(ᵔᵕᵔ)/",
 	"_vibecheck" => "╰( ͡° ͜ʖ ͡° )つ ︻╦╤─",
 	"_wtf" => "(._.)",
-	"_tux" =>",
+	"_tux" => ",
 			 .--.
 			|o_o |
 			|:_/ |
@@ -61,7 +65,7 @@ $emojis = array(
 		  (|     | )
 		 /'\_   _/`\
 		 \___)=(___/ ",
-	"_cat" =>"
+	"_cat" => "
 		░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
 		░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
 		░░░░░░█░█░▀░░░░░▀░░▀░░░░█░█░░░░░
@@ -76,47 +80,61 @@ $emojis = array(
 
 );
 
-$needle = '_'; //para fzr check do codigo pro emoji
-
-$host = "127.0.0.1"; //Ip da maquina
-$port = 2020; //Porta
-$date = DATE("H:i"); //Variavel para mostar a hora
+$host = "127.0.0.1"; //IP Server
+$port = 2020; //Porta Server
+$date = DATE("H:i"); //Hora do cliente
+echo "Bem vindo ao chat PHP!\nDigite texto para enviar\nDigite 'q' para sair\nDigite _emoji para ver lista emojis\n\n";
 $user = readline('Insira o seu nome: '); //Leitura do nome do cliente
 
-if(ctype_space($user) or $user="") //se usr n digiar texto
-	$user ="Guest User";
+if (ctype_space($user) or $user == "") //se user nao digiar texto
+	$user = "Guest User";
 
-while (true) {
+//socket_write($socket, "$user entrou no chat", 1024);	
+
+while (true){
+
+	echo "
+	 ██████╗██╗  ██╗ █████╗ ████████╗    ██████╗ ██╗  ██╗██████╗ 
+	██╔════╝██║  ██║██╔══██╗╚══██╔══╝    ██╔══██╗██║  ██║██╔══██╗
+	██║     ███████║███████║   ██║       ██████╔╝███████║██████╔╝
+	██║     ██╔══██║██╔══██║   ██║       ██╔═══╝ ██╔══██║██╔═══╝ 
+	╚██████╗██║  ██║██║  ██║   ██║       ██║     ██║  ██║██║     
+	╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝       ╚═╝     ╚═╝  ╚═╝╚═╝ \n\n";
 	
-	$ticker = readline('Digite algo ou clique na tecla "q" para sair: ');
-	if ($ticker == 'q') exit;
+	echo "\n";$ticker = readline('Msg: ');
+	//echo chr(27).chr(91).'H'.chr(27).chr(91).'J';  //limpa tela 
+	
+	if ($ticker == 'q') exit; //user sai do chat
+	elseif ($ticker == '_emoji') echo emojiMenu(); //apresenta menu de emojis
 
-	// Criação da socket
-	$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-
-	//Se a socket não for encontrada mostra um aviso
-	if ($socket === false)
-		echo "Socket creation failed!";
-
-	$result = socket_connect($socket, $host, $port);
-	if ($result === false)
-		echo "Socket connection failed!";
-
-	//Envia para o servidor e recebe a menssagem do outro utilizador
 	else {
-		echo "╔═══════════════════════════════════════════════════════╗";
+		// Criação da socket
+		$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
 
-		//troca os palavras de baixo de baixo calao, pelo censrua no array
-		$ticker = str_replace(array_keys($badwords), array_values($badwords), $ticker);
-		//troca os codigos para emojis
-		$ticker = str_replace(array_keys($emojis), array_values($emojis), $ticker);
+		//Se a socket não for encontrada mostra um aviso
+		if ($socket === false)
+			echo "Socket creation failed!";
 
-		socket_write($socket, "<$date> $user digitou --> $ticker", 1024);
-		$receber = socket_read($socket, 1024);
-		echo ("$receber \n");
+		$result = socket_connect($socket, $host, $port);
+		if ($result === false)
+			echo "Socket connection failed!";
 
-		echo socket_read($socket, 1024);
+		//Envia para o servidor e recebe a menssagem do outro utilizador
+		else {
+			echo "╔═══════════════════════════════════════════════════════╗";
 
-		echo "╚═══════════════════════════════════════════════════════╝";
+			//troca os palavras de baixo de baixo calao, pelo censrua no array
+			$ticker = str_replace(array_keys($badwords), array_values($badwords), $ticker);
+			//troca os codigos para emojis
+			$ticker = str_replace(array_keys($emojis), array_values($emojis), $ticker);
+
+			socket_write($socket, "<$date> $user digitou --> $ticker", 1024);
+			$receber = socket_read($socket, 1024);
+			echo ("$receber \n");
+
+			echo socket_read($socket, 1024);
+
+			echo "╚═══════════════════════════════════════════════════════╝";
+		}
 	}
 }
